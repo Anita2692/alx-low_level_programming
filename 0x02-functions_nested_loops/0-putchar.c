e0c715ba46dc72d575237182e43f56c71ddd1bb7
Anita2692 <zobeashiaanita@gmail.com>
2023-02-21 20:40:26 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
-* main - print _putchar
-* Return: 0.
-*/
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+**/
 
 int main(void)
 {
@@ -15,5 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+
+	return (0);
 }
+
