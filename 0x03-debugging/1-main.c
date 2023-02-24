4d83b9239f53d8cbd74ff0b304e5b3827729f79c
Anita2692 <zobeashiaanita@gmail.com>
2023-02-23 17:28:12 -0800

    like

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,11 +6,9 @@
  */
 
 int main(void)
-
 {
-	
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
@@ -19,6 +17,8 @@ int main(void)
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
